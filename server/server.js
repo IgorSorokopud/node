62123b2f16cdfd09b776eb29d1e3e66da8317070
Igor Sorokopud <igorztsorokopud@gmail.com>
2016-06-29 22:08:59 +0300

    add async server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+
+var http = require('http');
+var url = require('url');
+
+var server = new http.Server(function(reg, res) {
+
+    var urlParsed = url.parse(reg.url, true);
+
+    if (urlParsed.pathname == '/echo' && urlParsed.query.message) {
+        res.end(urlParsed.query.message);
+    } else {
+        res.statusCode = 404;
+        res.end('Page not found');
+    }
+});
+
+server.listen(1337, '127.0.0.1');
---
+++ serverAsync.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+
+    if (req.url == '/') {
+
+        fs.readFile('index.html', function (err, info) {
+            if (err) {
+                console.error(err);
+                res.statusCode = 500;
+                res.end('На сервере произошла ошибка');
+                return;
+            }
+
+            res.end(info);
+        });
+
+    } else { /*  404  */ }
+
+}).listen(3000);
